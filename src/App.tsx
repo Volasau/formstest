e69356028a8c768f41c7228719990eb847fe8af7
Ryhor Volsasu <g.volosov@email.com>
2023-11-30 01:24:25 +0400

    feat: new pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import style from './App.module.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home/Home';
+import FormUN from './components/FormUn/FormUn';
+import FormHook from './components/FormHook/FormHook';
+import Error from './components/pages/Error/Error';
+// import style from './App.module.css';
 
 function App() {
   return (
-    <>
-      <div className={style.title}>
-        <h3 className={style.text}>Forms</h3>
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/formun" element={<FormUN />} />
+      <Route path="/formhok" element={<FormHook />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
   );
 }
 
---
+++ src/redux/store.tsx
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+  reducer: {
+    // ваш редюсер здесь
+  },
+});
+
+export default store;
